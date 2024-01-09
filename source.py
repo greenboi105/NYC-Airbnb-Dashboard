f1265e75e98e137e95e02f9a9107875b5d3b55b1
greenboi105 <jyu5105@gmail.com>
2024-01-09 15:27:58 -0500

    reformatted some things

--- source.py
+++ source.py
@@ -14,26 +14,36 @@ server = app.server
 API_KEY = 'pk.eyJ1IjoiZ3JlZW5ib2kxMDUiLCJhIjoiY2xlMGo4aGZ0MDlyNTN2cWtlYnAyeTduNCJ9.22dna4Wm_VmFU1OKBXCFgQ'
 px.set_mapbox_access_token(API_KEY)
 
-# Load data into CSV 
+# Load data into CSV - we do so using pandas ability to read from a hosted raw CSV file
 url = 'https://raw.githubusercontent.com/greenboi105/python_nyc/main/AB_NYC_2019.csv'
+
+# The original dataset
 airbnb = pd.read_csv(url)
 
+# Filtering for only prices where the price is below 500 (this is done to remove extreme outliers)
 prices = airbnb[airbnb['price'] < 500]
+
+# Determine the boroughs of NYC 
 regions = prices["neighbourhood_group"].sort_values().unique()
 
-neighbourhood_distribution = px.scatter_mapbox(prices, lon='longitude', lat='latitude', color='price', template="plotly_dark", title="Prices in the Five Boroughs", height=700, color_continuous_scale="portland").update(layout=dict(title=dict(x=0.5)))
-density_distribution = px.violin(prices, x='neighbourhood_group', y='price', color='neighbourhood_group',box=True, title='Price Distribution of Neighbourhood Groups', template="plotly_dark", height=700).update(layout=dict(title=dict(x=0.5)))
-neighbourhood_group = px.scatter_mapbox(airbnb, lon='longitude', lat='latitude', color='availability_365', title="Availability in The Five Boroughs", template="plotly_dark", height=700, color_continuous_scale="delta", labels=dict(availability_365="Days Available Per Year")).update(layout=dict(title=dict(x=0.5)))
-neighbourhood_pricing = px.scatter(airbnb, x="room_type", y="price", color="neighbourhood_group", title="Room Type and Neighbourhood Pricing", template="plotly_dark", height=700).update(layout=dict(title=dict(x=0.5)))
-price_distribution = px.histogram(airbnb, x="price", title="Distribution of Airbnb Prices in NYC", color="neighbourhood_group", marginal='box', template="plotly_dark", height=700).update(layout=dict(title=dict(x=0.5)))
-
+# Processing to filter only the DataFrame with the most active host IDs
 top_host = airbnb.host_id.value_counts().head(10)
 top_host_df=pd.DataFrame(top_host)
 top_host_df.reset_index(inplace=True)
-top_host_df.rename(columns={'index':'Host_ID', 'host_id':'P_Count'}, inplace=True)
+top_host_df.rename(columns={'host_id':'Host_ID', 'count':'P_Count'}, inplace=True)
 
-hosts = [str(host) for host in top_host_df["Host_ID"].unique()]
-host_plot = px.bar(top_host_df, hosts, y='P_Count', color="P_Count", title="Host IDs with Most Rentals", template="plotly_dark", labels=dict(x="Host Ids", P_Count="Available Listings"), height=700).update(layout=dict(title=dict(x=0.5)))
+# Plots
+neighbourhood_distribution = px.scatter_mapbox(prices, lon='longitude', lat='latitude', color='price', template="plotly_dark", title="Prices in the Five Boroughs", height=700, color_continuous_scale="portland").update(layout=dict(title=dict(x=0.5)))
+
+density_distribution = px.violin(prices, x='neighbourhood_group', y='price', color='neighbourhood_group',box=True, title='Price Distribution of Neighbourhood Groups', template="plotly_dark", height=700).update(layout=dict(title=dict(x=0.5)))
+
+neighbourhood_group = px.scatter_mapbox(airbnb, lon='longitude', lat='latitude', color='availability_365', title="Availability in The Five Boroughs", template="plotly_dark", height=700, color_continuous_scale="delta", labels=dict(availability_365="Days Available Per Year")).update(layout=dict(title=dict(x=0.5)))
+
+neighbourhood_pricing = px.scatter(airbnb, x="room_type", y="price", color="neighbourhood_group", title="Room Type and Neighbourhood Pricing", template="plotly_dark", height=700).update(layout=dict(title=dict(x=0.5)))
+
+price_distribution = px.histogram(airbnb, x="price", title="Distribution of Airbnb Prices in NYC", color="neighbourhood_group", marginal='box', template="plotly_dark", height=700).update(layout=dict(title=dict(x=0.5)))
+
+host_plot = px.bar(top_host_df, [str(host) for host in top_host_df["Host_ID"].unique()], y='P_Count', color="P_Count", title="Host IDs with Most Rentals", template="plotly_dark", labels=dict(x="Host Ids", P_Count="Available Listings"), height=700).update(layout=dict(title=dict(x=0.5)))
 
 colors = {
     'background': '#111111',
@@ -44,7 +54,12 @@ prompt_description = """
     Analysis of factors influencing prices of Airbnb rentals in New York City. New York City is one of the most global cities in the world, and attracts tenants from all corners of the globe.
 """
 
-# The structure of the app is a tree
+"""
+The structure of the app is a tree - this is where we display the plots generated from Plotly using Dash.
+
+We have the original heading text, then the figures with which we want to display information.
+"""
+
 app.layout = html.Div(children=[
     html.Div(
         children=[
